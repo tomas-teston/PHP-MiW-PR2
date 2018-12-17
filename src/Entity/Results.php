9e625b2ccba98dbc3b6c5c253825f2e19069e4b4
Tomas Muñoz Testón <tomini18@hotmail.com>
2018-12-17 20:37:30 +0100

    created users and results Entities.

---
+++ src/Entity/Results.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Results
+ *
+ * @ORM\Table(name="results", indexes={@ORM\Index(name="FK_USER_ID_idx", columns={"user_id"})})
+ * @ORM\Entity
+ */
+class Results implements JsonSerializable
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="result", type="integer", nullable=false)
+     */
+    private $result;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="time", type="datetime", nullable=false)
+     */
+    private $time;
+
+    /**
+     * @var \Users
+     *
+     * @ORM\ManyToOne(targetEntity="Users")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     * })
+     */
+    private $user;
+
+    /**
+     * Results constructor.
+     * @param int $id
+     * @param int $result
+     * @param DateTime $time
+     * @param Users $user
+     */
+    public function __construct(int $id, int $result, DateTime $time, Users $user)
+    {
+        $this->id = $id;
+        $this->result = $result;
+        $this->time = $time;
+        $this->user = $user;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Results
+     */
+    public function setId(int $id): Results
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getResult(): int
+    {
+        return $this->result;
+    }
+
+    /**
+     * @param int $result
+     * @return Results
+     */
+    public function setResult(int $result): Results
+    {
+        $this->result = $result;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getTime(): DateTime
+    {
+        return $this->time;
+    }
+
+    /**
+     * @param DateTime $time
+     * @return Results
+     */
+    public function setTime(DateTime $time): Results
+    {
+        $this->time = $time;
+        return $this;
+    }
+
+    /**
+     * @return Users
+     */
+    public function getUser(): Users
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param Users $user
+     * @return Results
+     */
+    public function setUser(Users $user): Results
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'result' => [
+                'id' => $this->getId(),
+                'result' => $this->getResult(),
+                'time' => $this->getTime(),
+                'user' => $this->getUser()
+            ]
+        ];
+    }
+
+    /**
+     * The __toString method allows a class to decide how it will react when it is converted to a string.
+     *
+     * @return string
+     * @link https://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.tostring
+     */
+    public function __toString()
+    {
+        return "id = " . $this->getId() . ", result = " . $this->getResult() .
+            ", idUser = " . $this->getUser()->getId();
+    }
+
+
+}
---
+++ src/Entity/Users.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Users
+ *
+ * @ORM\Table(name="users", uniqueConstraints={@ORM\UniqueConstraint(name="IDX_UNIQ_EMAIL", columns={"email"}), @ORM\UniqueConstraint(name="IDX_UNIQ_USER", columns={"username"})})
+ * @ORM\Entity
+ */
+class Users implements JsonSerializable
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=40, nullable=false)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=60, nullable=false)
+     */
+    private $email;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="enabled", type="boolean", nullable=false)
+     */
+    private $enabled;
+
+    /**
+     * @var bool|null
+     *
+     * @ORM\Column(name="admin", type="boolean", nullable=true)
+     */
+    private $admin = '0';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=60, nullable=false)
+     */
+    private $password;
+
+    /**
+     * Users constructor.
+     * @param int $id
+     * @param string $username
+     * @param string $email
+     * @param bool $enabled
+     * @param bool|null $admin
+     * @param string $password
+     */
+    public function __construct(int $id, string $username, string $email, bool $enabled, ?bool $admin, string $password)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->email = $email;
+        $this->enabled = $enabled;
+        $this->admin = $admin;
+        $this->password = $password;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Users
+     */
+    public function setId(int $id): Users
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     * @return Users
+     */
+    public function setUsername(string $username): Users
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return Users
+     */
+    public function setEmail(string $email): Users
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param bool $enabled
+     * @return Users
+     */
+    public function setEnabled(bool $enabled): Users
+    {
+        $this->enabled = $enabled;
+        return $this;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getAdmin(): ?bool
+    {
+        return $this->admin;
+    }
+
+    /**
+     * @param bool|null $admin
+     * @return Users
+     */
+    public function setAdmin(?bool $admin): Users
+    {
+        $this->admin = $admin;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return Users
+     */
+    public function setPassword(string $password): Users
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'User' => [
+                'id' => $this->getId(),
+                'username' => $this->getUsername(),
+                'email' => $this->getEmail(),
+                'enabled' => $this->isEnabled(),
+                'admin' => $this->getAdmin()
+            ]
+        ];
+    }
+
+    /**
+     * The __toString method allows a class to decide how it will react when it is converted to a string.
+     *
+     * @return string
+     * @link https://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.tostring
+     */
+    public function __toString()
+    {
+        return "[id = " . $this->getId() . ", username = " . $this->getUsername() .
+            ", email = " . $this->getEmail() . ", enabled = " . $this->isEnabled() .
+            ", admin = " . $this->getAdmin();
+    }
+
+
+}
